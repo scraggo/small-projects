e844080a4c6af2fcf3e80f482882cac59a553e64
Dave Cohen <dcohen18@gmail.com>
2017-11-15 11:10:05 -0600

    Rename files, Add node implementation of exercises

---
+++ exercises-javascript/EloquentJavaScript/05_01flatten.js
@@ -0,0 +1,18 @@
+/*
+Flattening
+
+Use the reduce method in combination with the concat method to “flatten” an array of arrays into a single array that has all the elements of the input arrays.
+*/
+
+var arrays = [[1, 2, 3], [4, 5], [6]];
+// Your code here.
+// → [1, 2, 3, 4, 5, 6]
+
+function flatten(arr) {
+  return arr.reduce( function(a, item) {
+    return a.concat(item);
+  }, []
+  );
+}
+
+console.log(flatten(arrays));
