1a515a3d1d4016c5ff3821a9f7a6ff03dd163135
Dave Cohen <scraggo@gmail.com>
2020-07-17 22:13:05 -0500

    Update readme, docs

---
+++ small-projects-javascript/bookmarks_to_csv/docs/todo.md
@@ -0,0 +1,9 @@
+# To do
+
+- Change description of repo
+- Change repo instructions in readme
+- prevent overwrite of output if file exists
+
+Done
+
+- [x] Add CLI usage if arguments mistyped
--- small-projects-javascript/bookmarks_to_csv/src/utils/commander.js
+++ small-projects-javascript/bookmarks_to_csv/src/utils/commander.js
@@ -3,7 +3,10 @@ import commander from 'commander';
 commander
   .version('0.0.1')
   .requiredOption('-i, --input <path>', 'Path to bookmarks html file')
-  .option('-o, --output <path>', 'Path to output csv file (optional)')
+  .option(
+    '-o, --output <path>',
+    'Path to output csv file (optional). If not specified, <inputName.csv> will be used.'
+  )
   .parse(process.argv);
 
 export default commander;
