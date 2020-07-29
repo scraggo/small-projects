4bcf4ac040f8b46c0d2734afc253730ff44abe41
Dave Cohen <scraggo@gmail.com>
2020-07-29 18:33:05 -0500

    Allow resolve "~"

--- small-projects-javascript/question-prompter/src/get-config.js
+++ small-projects-javascript/question-prompter/src/get-config.js
@@ -1,8 +1,9 @@
-import { existsAsync, readFileAsync } from './utils/fs';
+import { existsAsync, readFileAsync, resolvePath } from './utils/fs';
 
 export default commander => {
   // config is required, or we don't get to this point
-  const { config: configPath } = commander;
+  const { config } = commander;
+  const configPath = resolvePath(config);
   return existsAsync(configPath)
     .then(() => readFileAsync(configPath, { encoding: 'utf8' }))
     .then(async json => {
