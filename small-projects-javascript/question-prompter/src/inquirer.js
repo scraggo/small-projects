7bce806a5490ba866b914aee99a0865623fec0bc
Dave Cohen <scraggo@gmail.com>
2020-07-29 18:34:22 -0500

    Add "content" key to json

--- small-projects-javascript/question-prompter/src/inquirer.js
+++ small-projects-javascript/question-prompter/src/inquirer.js
@@ -1,8 +1,8 @@
 import inquirer from 'inquirer';
+import { openWithVSCode } from './utils/exec';
 
-import exampleData from './example/example.json';
-
-const findFromData = choice => exampleData.find(data => data.name === choice);
+const findFromData = (choice, userData) =>
+  userData.find(data => data.name === choice);
 
 const secondLevel = {
   answerQuestions: {
@@ -15,12 +15,12 @@ const secondLevel = {
             type: 'list',
             name: 'whichQuestions',
             message: 'Which questions do you want to answer?',
-            choices: userData
+            choices: userData.filter(entry => Boolean(entry.questions))
           }
         ])
         .then(prompt => {
           const choice = prompt.whichQuestions;
-          const questionsToAnswer = findFromData(choice).questions;
+          const questionsToAnswer = findFromData(choice, userData).questions;
           return inquirer.prompt(questionsToAnswer);
         })
         .then(answersToQs => {
@@ -37,13 +37,14 @@ const secondLevel = {
             type: 'list',
             name: 'whichNote',
             message: 'What note do you want to read?',
-            choices: userData
+            choices: userData.filter(entry => Boolean(entry.path))
           }
         ])
         .then(prompt => {
           const choice = prompt.whichNote;
-          const noteToView = findFromData(choice).path;
-          console.log(noteToView);
+          const noteToView = findFromData(choice, userData).path;
+          console.log(`Opening ${noteToView}...`);
+          openWithVSCode(noteToView);
         })
   }
 };
@@ -58,8 +59,9 @@ const prompt1 = [
 ];
 
 export default config => {
+  const { content } = config; // todo: later, we can add "openWith"
   return inquirer.prompt(prompt1).then(answers => {
     const choice = answers.topLevel;
-    return secondLevel[choice].cb(config);
+    return secondLevel[choice].cb(content);
   });
 };
