2b700c71a0e312038a8994a2ac74fc1ae24f278a
Dave Cohen <scraggo@gmail.com>
2021-10-30 22:38:20 -0500

    Update type_function

--- small-projects-javascript/type_function.js
+++ small-projects-javascript/type_function.js
@@ -1,20 +1,31 @@
+// @ts-check
 
-// adds 'array' and 'set' to typeof function
+/**
+ * adds 'array', 'null', 'set' to typeof function
+ * @param {unknown} item
+ */
 function type(item) {
+  if (item === null) {
+    return 'null';
+  }
   if (Array.isArray(item)) {
     return 'array';
   }
   if (item instanceof Set) {
     return 'set';
   }
-  return typeof item; 
+  return typeof item;
 }
 
-console.log(type([1,2,3]));
-console.log(type({a: 2}));
+console.log(type([1, 2, 3]));
+console.log(type({ a: 2 }));
 console.log(type(2));
 console.log(type('a'));
-console.log(typeof function(){return;});
+console.log(
+  typeof function () {
+    return;
+  }
+);
 
-let setTest = new Set([1,2,3]);
+let setTest = new Set([1, 2, 3]);
 console.log(type(setTest));
