fbad07befa5f050e7c18bb93773000dd9999fa00
Dave Cohen <scraggo@gmail.com>
2023-07-02 18:56:25 -0500

    add generate-all

---
+++ small-projects-javascript/karabiner-source/generate-all.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+node basic/number-layer.js > basic/number-layer.json
+node basic/shift-layer.js > basic/shift-layer.json
+node basic/windows-modifiers.js > basic/windows-modifiers.json
+
+node complex/select-profile.js > complex/select-profile.json
+
+node function_keys/func.js > function_keys/func.json
